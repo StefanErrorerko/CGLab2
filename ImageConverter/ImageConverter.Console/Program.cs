d4d30a41437b2e02f7700a49f18bbde52580c673
bohdankonopolskyi <bohdan_konopolskyi@icloud.com>
2023-05-09 15:06:59 +0300

    CLI Parser

--- ImageConverter/ImageConverter.Console/Program.cs
+++ ImageConverter/ImageConverter.Console/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ImageConverter.Console;
+using ImageConverter.Orchestrator;
+
+const string sourceFlag = "source";
+const string objectiveExtensionFlag = "goal-format";
+const string outputFlag = "output";
+
+var parser = new CommandLineParser(args);
+
+var objectiveExtension = parser.GetArgument(objectiveExtensionFlag);
+var source = parser.GetArgument(sourceFlag);
+var output = parser.GetArgument(outputFlag);
+
+ConverterOrchestrator converter = new ConverterOrchestrator();
+converter.Convert(source, objectiveExtension);
+ Console.ReadKey();
