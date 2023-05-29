df67befc08c66bd6e75c4aa9e0bfff2eccc05bdb
Bohdan Konopolskyi <bohdan_konopolskyi@icloud.com>
2023-05-29 21:21:07 +0300

    CommandLineParser implementation

---
+++ RayCasting/RayCasting.Console/CommandLineParser.cs
@@ -0,0 +1,34 @@
+namespace RayCasting.Console;
+
+internal class CommandLineParser
+{
+    private readonly Dictionary<string, string> _arguments;
+
+    public CommandLineParser(string[] args)
+    {
+        _arguments = new Dictionary<string, string>();
+
+        foreach (var arg in args)
+        {
+            var equalsIndex = arg.IndexOf('=');
+            if (equalsIndex == -1) throw new ArgumentException($"Invalid argument: {arg}");
+
+            var key = arg.Substring(0, equalsIndex);
+            var value = arg.Substring(equalsIndex + 1);
+
+            _arguments[key] = value;
+        }
+    }
+
+    public string GetArgument(string key)
+    {
+        if (!_arguments.TryGetValue(key, out var value)) throw new ArgumentException($"Argument not found: {key}");
+
+        return value;
+    }
+
+    public bool HasArgument(string key)
+    {
+        return _arguments.ContainsKey(key);
+    }
+}
--- RayCasting/RayCasting.Console/Program.cs
+++ RayCasting/RayCasting.Console/Program.cs
@@ -1,7 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using RayCasting.Console;
 using RayCasting.Core.Objects;
 using RayCasting.Core.ObjLoader;
+
+const string sourceFlag = "source";
+const string outputFlag = "output";
+
+var parser = new CommandLineParser(args);
+
+var source = parser.GetArgument(sourceFlag);
+var output = parser.GetArgument(outputFlag);
 
 Mesh mesh = ObjLoader.LoadObj(@"C:\Users\kadde\Downloads\cow.obj");
 mesh.Print();
